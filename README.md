1561a61ff158b4bdd862d6c5fac30618170c81ef
Søren Louv-Jansen <sorenlouv@gmail.com>
2025-03-03 13:43:07 +0100

    Add readme and other fixes

---
+++ README.md
@@ -0,0 +1,40 @@
+# LLM Debug Proxy
+
+**LLM Debug Proxy** is a proxy designed for debugging LLM requests. It forwards requests to your LLM of choice (OpenAI, Llama), logs full request and response bodies (including streaming responses and tool calls), and displays useful debugging information.
+
+> **Note:** This project is intended for debugging and development only. It is not optimized for production use.
+
+## Installation
+
+1. **Clone the repository:**
+
+```sh
+git clone https://github.com/elastic/llm-debug-proxy.git
+cd llm-debug-proxy
+npm install
+```
+
+## Usage
+
+### Start proxy
+
+```sh
+npm start
+```
+
+### Send requests to the proxy
+
+The proxy listens on port 3000 by default. To forward a request to a target endpoint, include the URL-encoded target URL as a query parameter `target_url`
+
+```sh
+curl http://localhost:3000?target_url=http%3A%2F%2Flocalhost%3A11434%2Fapi%2Fchat -d '{
+  "model": "llama3.2",
+  "messages": [
+    {
+      "role": "user",
+      "content": "Why is the sky blue?"
+    }
+  ],
+  "stream": true
+}'
+```
---
+++ src/formatters.ts
@@ -0,0 +1,115 @@
+import express from 'express';
+import OpenAI from 'openai';
+import { URL } from 'url';
+
+export function parseTargetUrl(targetUrlParam: string | undefined) {
+  if (!targetUrlParam) {
+    console.log('target_url query parameter is required');
+    return;
+  }
+
+  const decodedUrl = decodeURIComponent(targetUrlParam);
+  try {
+    return new URL(decodedUrl);
+  } catch (err) {
+    console.log(`target_url query parameter is invalid: "${decodedUrl}"`);
+    return;
+  }
+}
+
+export function formatResponseBody(res: express.Response, allResponseChunks: string) {
+  const isStreamedResponse = res.getHeader('content-type') === 'text/event-stream' || res.getHeader('transfer-encoding') === 'chunked'
+  if (!isStreamedResponse) {
+    try {
+      return JSON.stringify(JSON.parse(allResponseChunks), null, 2)
+    } catch (e) {
+      return allResponseChunks;
+    }
+  }
+
+  const lines = allResponseChunks
+    .split(/\r?\n/) // Handle both \n and \r\n
+    .map(line => line.trim())
+
+
+  const hasSSEChunks = lines.some(line => line.startsWith('data:'));
+
+  console.log(`Chunks: ${allResponseChunks.length}`);
+  console.log(`Lines: ${lines.length}`);
+  console.log(`Server-sent events (SSE): ${hasSSEChunks.valueOf()}`);
+
+  if (!hasSSEChunks) {
+    return lines.map(line => {
+      try {
+        const parsedLine = JSON.parse(line);
+        return parsedLine.message.content;
+      } catch (e) {
+        return line;
+      }
+    }).join('')
+  }
+
+  const parsedSSEChunks = lines.filter(line => line.startsWith('data:') && line !== 'data: [DONE]') // Filter out non-data and [DONE]
+    .map(line => JSON.parse(line.slice(5).trim()));
+
+  const isOpenAIChunk = parsedSSEChunks.some(chunk => chunk.object === 'chat.completion.chunk' && chunk.choices)
+  if (!isOpenAIChunk) {
+    return JSON.stringify(parsedSSEChunks, null, 2);
+  }
+
+  const mergedMessage = mergeOpenAIChunks(parsedSSEChunks);
+  return JSON.stringify(mergedMessage, null, 2);
+}
+
+export function formatRequestBody(requestData: string) {
+  try {
+    const parsed = JSON.parse(requestData);
+    return JSON.stringify(parsed, null, 2);
+    // return JSON.stringify(omit(parsed, 'tools'), null, 2);
+  } catch (e) {
+    return requestData;
+  }
+}
+
+interface UnvalidatedMessage {
+  content: string;
+  tool_calls: NonNullable<OpenAI.ChatCompletionChunk['choices'][0]['delta']['tool_calls']>;
+}
+
+type Delta = OpenAI.ChatCompletionChunk['choices'][0]['delta'];
+export const mergeOpenAIChunks = (chunks: OpenAI.ChatCompletionChunk[]) => {
+  const message = chunks.reduce<UnvalidatedMessage>(
+    (prev, chunk) => {
+      const delta = chunk.choices[0]?.delta ?? {};
+
+      prev.content += delta?.content ?? '';
+      delta.tool_calls?.forEach((toolCall) => {
+        let prevToolCall = prev.tool_calls[toolCall.index];
+        if (!prevToolCall) {
+          prev.tool_calls[toolCall.index] = {
+            function: {
+              name: '',
+              arguments: '',
+            },
+            index: 0,
+            id: '',
+          };
+
+          prevToolCall = prev.tool_calls[toolCall.index];
+        }
+
+        // @ts-expect-error
+        prevToolCall.function.name += toolCall.function.name ?? '';
+        // @ts-expect-error
+        prevToolCall.function.arguments += toolCall.function.arguments ?? '';
+        prevToolCall.index += toolCall.index ?? '';
+        prevToolCall.id += toolCall.id ?? '';
+      });
+
+      return prev;
+    },
+    { content: '', tool_calls: [] }
+  );
+
+  return message;
+};
